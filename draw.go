9ad81ab55c3e40681385a331437c20e8d1601995
Justin Flude <jflude@users.noreply.github.com>
2016-07-26 22:56:06 -0500

    scale by 3, invert colours

--- draw.go
+++ draw.go
@@ -3,18 +3,29 @@ package main
 import (
 	"image"
 	"image/color"
+	"image/draw"
 )
 
 var display = image.NewRGBA(image.Rect(0, 0, 1000, 1000))
 var offset = image.Pt(400, 400)
 
+func init() {
+	draw.Draw(display, display.Bounds(), &image.Uniform{C: color.White}, image.ZP, draw.Src)
+}
+
 func drawCell(x, y int, cell int8) {
 	var c color.Color
 	if isAlive(cell) {
-		c = color.White
-	} else {
 		c = color.Black
+	} else {
+		c = color.White
 	}
 
-	display.Set(x+offset.X, y+offset.Y, c)
+	x = 3*x + offset.X
+	y = 3*y + offset.Y
+
+	display.Set(x, y, c)
+	display.Set(x+1, y, c)
+	display.Set(x, y+1, c)
+	display.Set(x+1, y+1, c)
 }
